cee16ecb2b60b7aea2ed7c2024de86bcf71fe454
Esther Quirós <estherbequiros@gmail.com>
2025-11-20 11:24:27 +0100

    dates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,69 @@
 import "../styles/App.scss";
 
+
 function App() {
+  /* DATOS:
+  -Posición -> Valor iniial: 0 (ó 1)
+  -Num de galletas: 3
+  -Num de Huevitos: 3
+  -Num de Ranitas: 3
+
+  EVENTOS Click botón "Lanzar dado"
+
+  1. Gen número aleatorio debe estar entre 1 y 4
+  2. Condicional 
+  -Dado es 4 -> Groru avanza una posición -> Cambiar la vari-estado de la posición
+  -Dado es 1, 2 o 3 -> Grogu descarga una mercancía -> Cambiar la vari-estado de la mercancía que corresponda
+
+  EVENTO Click botón "Reiniciar Juego"
+
+  1. Volver a poner los datos a su valor inicial.
+  */
+
+
+  
   return (
     <div>
-      <header className="header">
-        <h1 className="title">Plantilla React</h1>
-      </header>
-      <main className="main">Holis Adalabers!!!</main>
+       <header>
+      <h1>¡Cuidado con Grogu!</h1>
+    </header>
+    <main className="page">
+      <section className="board">
+        <div className="cell"><div className="grogu">👣</div></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </section>
+
+      <section>
+        <button className="dice">Lanzar Dado</button>
+        <div className="game-status">En curso</div>
+      </section>
+
+      <section className="goods-container">
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+      </section>
+      <section className="goods-container">
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+      </section>
+      <section className="goods-container">
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+      </section>
+      <section>
+        <button className="restart-button">Reiniciar Juego</button>
+      </section>
+    </main>
     </div>
+    
   );
 }
 
